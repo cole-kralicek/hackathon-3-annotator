169f1fd6a963aa0c96337fee9364402f076e3d32
Cole Kralicek <kralicekcole61@gmail.com>
2024-08-23 21:09:58 -0700

    fixed fonts, added Header component, and clerk middleware

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+'use client'; 
+
+const Header = () => {
+    return (
+        <div>
+            <header>Hello from Header</header>
+        </div>
+    )
+}
+
+export default Header; 
---
+++ styles/fonts.ts
@@ -0,0 +1,10 @@
+import localFont from 'next/font/local'
+
+const LTRemarkRegular = localFont({ src: '/LTRemark-Regular.otf' });
+
+const LTRemarkBold = localFont({ src: '/LTRemark-Bold.otf' });
+
+const LTRemarkItalic = localFont({ src: '/LTRemark-Italic.otf' })
+
+ 
+export { LTRemarkRegular, LTRemarkItalic, LTRemarkBold }
